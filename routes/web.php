7c49273d5dc9a9084fdec98ee6a3a4908ea72381
Wandi <karinahnis48@gmail.com>
2025-01-03 17:20:11 +0700

    Perubahan di routers/web. Menghapus routes category dan login yang duplikat

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,13 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LoginController; 
 use App\Http\Controllers\UserController; 
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
 
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 })->name('home');
 
-Route::get('/login', function () {
-    return view('auth.login'); 
-})->name('login.form');
 
-Route::post('/login', [UserController::class, 'login'])->name("login");
 Route::get('register', [UserController::class, 'register'])->name('register');
 Route::post('register', [UserController::class, 'register_action'])->name('register.action');
 Route::get('login', [UserController::class, 'login'])->name('login');
@@ -24,7 +19,6 @@ Route::post('login', [UserController::class, 'login_action'])->name('login.actio
 Route::get('password', [UserController::class, 'password'])->name('password');
 Route::post('password', [UserController::class, 'password_action'])->name('password.action');
 Route::get('logout', [UserController::class, 'logout'])->name('logout');
-
 
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
@@ -34,16 +28,4 @@ Route::get('logout', [UserController::class, 'logout'])->name('logout');
     Route::delete('/products/{products}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 
-
-
-
-
-
-
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-Route::delete('categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
-Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
 
