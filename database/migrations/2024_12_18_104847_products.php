97f502d5c2fd6094d5a7c1efd06d6021cc445e73
Wandi <karinahnis48@gmail.com>
2025-01-15 21:31:03 +0700

    perubahan di produk controller

--- database/migrations/2024_12_18_104847_products.php
+++ database/migrations/2024_12_18_104847_products.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->BigInteger('category_id');
+            $table->BigInteger('category_id')->constrained('categories')->onDelete('cascade');
             $table->decimal('price', 10, 2);
             $table->integer('stock_available');
             $table->timestamps();
---
+++ database/migrations/2025_01_14_042657_add_category_id_to_products_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            if (!Schema::hasColumn('products', 'category_id')) {
+                $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            }           });
+    }
+
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
+    }
+};
